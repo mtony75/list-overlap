76de732cbee2acabedd90f99efc6a1a9ba6490ee
Tony Fleming <mtony75@gmail.com>
2019-06-10 13:16:54 -0400

    added pseudo random list generator function

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import random
 method to generate the initial two list of different lengths. '''
 
 ''' createList function used to create random list of different lenghts '''
-def createList(length):
+def createList():
+    newList = random.sample(range(1,200), random.randint(7,20))
+    return newList
+
+# Function to compare the two list and put elements in common in a
+# third list
+def listCompare(first, second, empty):
     pass
 
 # Program Flow
