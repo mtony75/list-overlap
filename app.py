542dc51809dfe0b96d3179eaf12f776c85898fef
Tony Fleming <mtony75@gmail.com>
2019-06-10 12:55:49 -0400

    Initial pseudocode

---
+++ app.py
@@ -0,0 +1,18 @@
+import random
+
+''' Figure out the overlap in two lists using the random.sample 
+method to generate the initial two list of different lengths. '''
+
+''' createList function used to create random list of different lenghts '''
+def createList(length):
+    pass
+
+# Program Flow
+'''
+Create List one (random length)
+Create list two (random lenght)
+Init empty list
+Compare lists "Method" overLapList = listCompare(fist, second, empty)
+Print overlap list
+'''
+
